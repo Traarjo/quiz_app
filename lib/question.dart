1544b604cbcee0f36e6504dd799683c8a2d93a42
Tara Jørgensen <traarj@gmail.com>
2020-02-03 16:40:54 +0100

    styled text

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity, //takes as much width as it can get
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      )
+    );
   }
 }

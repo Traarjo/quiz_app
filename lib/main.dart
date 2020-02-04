c2a47211ddf0126e8ef698d0c329eb00fff656fb
Tara Jørgensen <traarj@gmail.com>
2020-02-04 16:16:28 +0100

    added finishing message

--- lib/main.dart
+++ lib/main.dart
@@ -13,42 +13,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite colour?',
+      'answers' : ['Red', 'Green', 'Yellow', 'Blue'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers' : ['Cat', 'Dog', 'Rabbit', 'Hamster'],
+    },
+    {
+      'questionText': 'What\'s your favourite music genre?',
+      'answers' : ['Pop', 'Rock', 'Rap', 'Country'],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex ++;
     });
+
     print(_questionIndex);
+
+    if (_questionIndex < questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favourite colour?',
-        'answers' : ['Red', 'Green', 'Yellow', 'Blue'],
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers' : ['Cat', 'Dog', 'Rabbit', 'Hamster'],
-      },
-      {
-        'questionText': 'What\'s your favourite music genre?',
-        'answers' : ['Pop', 'Rock', 'Rap', 'Country'],
-      },
-    ];
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
           title: Text('My First App'),
       ),
-      body: Column(
+      body: _questionIndex < questions.length ? Column(
         children: [
           Question(questions[_questionIndex]['questionText']),
           ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
             return Answer(_answerQuestion, answer);
           }).toList()
-      ],),
+        ],
+      ) : Center (child: Text('Finished!')),
     ));
   }
 }
